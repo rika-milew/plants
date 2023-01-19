6acb2939c8bd0b800ad262a3023f089c91e3165c
Erika Gumerova <95300271+erikagumerova@users.noreply.github.com>
2023-01-19 21:59:01 +0300

    Delete script1.js

--- js/script1.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('.header__burger,.header__menu').click(function(event) {
-        $('.header__burger, .header__menu').toggleClass("active");
-        $('body').toggleClass("lock");  
-        })
-    });
